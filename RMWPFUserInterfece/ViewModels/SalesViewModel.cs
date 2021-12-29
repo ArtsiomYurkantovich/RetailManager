7a40cdac78e5de2d1f61000164ef759e21626942
Artsiom_Yurkantovich <artsiomyurkantovich@gmail.com>
2021-12-29 23:00:11 +0300

    Sales Page Creation

---
+++ RMWPFUserInterfece/ViewModels/SalesViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Caliburn.Micro;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RMWPFUserInterfece.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected
+
+                return output;
+            }
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected
+
+                return output;
+            }
+        }
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure there is something in the cart
+
+                return output;
+            }
+        }
+        public string SubTotal
+        {
+            get
+            {
+                //TODO calculation subtotal
+                return "$0.00";
+            }
+        }
+
+        public string Total
+        {
+            get
+            {
+                //TODO calculation total
+                return "$0.00";
+            }
+        }
+
+        public string Tax
+        {
+            get
+            {
+                //TODO calculation tax
+                return "$0.00";
+            }
+        }
+        public void AddToCart()
+        {
+
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public void CheckOut()
+        {
+
+        }
+
+    }
+}
