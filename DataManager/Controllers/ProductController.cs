dd960cf054de028d2e6cb7342347361fd6021840
Artsiom_Yurkantovich <artsiomyurkantovich@gmail.com>
2021-12-30 01:46:44 +0300

    Add data product in DB

---
+++ DataManager/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Internal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DataManager.Controllers
+{
+    public class ProductController : ApiController
+    {
+        [Authorize]
+        public List<ProductModel> Get()
+        {
+
+            ProductData data = new ProductData();
+            return data.GetProducts();
+        }
+    }
+}
