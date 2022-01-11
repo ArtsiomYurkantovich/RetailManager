a719ae502c47b56eace57eb899ce92560acc9890
Artsiom_Yurkantovich <artsiomyurkantovich@gmail.com>
2022-01-11 19:19:01 +0300

    Revert "Revert "Wire Up the WPF Shopping Cart""
    This reverts commit e85b95e9d2b3ad99c436724c2c63e600f1486553.

--- RMWPFUserInterfece/ViewModels/SalesViewModel.cs
+++ RMWPFUserInterfece/ViewModels/SalesViewModel.cs
@@ -38,8 +38,19 @@ namespace RMWPFUserInterfece.ViewModels
                 NotifyOfPropertyChange(() => Products);
             }
         }
+        public ProductModel _selectedProduct { get; set; }
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
 
-        private int _itemQuantity;
+            }
+        }
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -48,18 +59,22 @@ namespace RMWPFUserInterfece.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
+
+
             }
         }
 
-        private BindingList<ProductModel> _cart;
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
 
-        public BindingList<ProductModel> Cart
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
             {
                 _cart = value;
                 NotifyOfPropertyChange(() => Cart);
+
             }
         }
 
@@ -69,7 +84,10 @@ namespace RMWPFUserInterfece.ViewModels
             {
                 bool output = false;
 
-                //Make sure something is selected
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
 
                 return output;
             }
@@ -101,8 +119,12 @@ namespace RMWPFUserInterfece.ViewModels
         {
             get
             {
-                //TODO calculation subtotal
-                return "$0.00";
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+                return subTotal.ToString("C");
             }
         }
 
@@ -125,11 +147,32 @@ namespace RMWPFUserInterfece.ViewModels
         }
         public void AddToCart()
         {
-
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+            
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+            NotifyOfPropertyChange(() => Cart);
         }
 
         public void RemoveFromCart()
         {
+            NotifyOfPropertyChange(() => SubTotal);
 
         }
 
