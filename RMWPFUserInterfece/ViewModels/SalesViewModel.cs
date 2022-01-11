410b3451db1fe31ea4f6ae9fdc5bd97e8f8adf04
Artsiom_Yurkantovich <artsiomyurkantovich@gmail.com>
2022-01-11 23:36:26 +0300

    First Refactoring

--- RMWPFUserInterfece/ViewModels/SalesViewModel.cs
+++ RMWPFUserInterfece/ViewModels/SalesViewModel.cs
@@ -150,13 +150,17 @@ namespace RMWPFUserInterfece.ViewModels
             decimal taxAmount = 0;
             decimal taxRate = _configHelper.GetTaxRate() / 100;
 
-            foreach (var item in Cart)
-            {
-                if (item.Product.IsTaxable)
-                {
-                    taxAmount = (item.Product.RetailPrice * item.QuantityInCart * taxRate);
-                }
-            }
+            taxAmount = Cart
+                .Where(x => x.Product.IsTaxable)
+                .Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate);
+
+            //foreach (var item in Cart)
+            //{
+            //    if (item.Product.IsTaxable)
+            //    {
+            //        taxAmount = (item.Product.RetailPrice * item.QuantityInCart * taxRate);
+            //    }
+            //}
             return taxAmount;
         }
 
