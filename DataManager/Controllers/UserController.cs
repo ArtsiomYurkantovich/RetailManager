196578c503a4965b1d50cd6f6f7516a2953af624
Artsiom_Yurkantovich <artsiomyurkantovich@gmail.com>
2022-01-31 23:20:28 +0300

    Adding Managing User Roles

--- DataManager/Controllers/UserController.cs
+++ DataManager/Controllers/UserController.cs
@@ -14,6 +14,7 @@ namespace DataManager.Controllers
 {
     public class UserController : ApiController
     {
+        #region GetById
         [Authorize]
         public UserModel GetById()
         {
@@ -21,7 +22,9 @@ namespace DataManager.Controllers
             UserData data = new UserData();
             return data.GetUserById(userId).First();
         }
+        #endregion
 
+        #region GetAllUsers
         [Authorize(Roles ="Admin")]
         [HttpGet]
         [Route("api/User/Admin/GetAllUsers")]
@@ -54,5 +57,52 @@ namespace DataManager.Controllers
             }
             return output;
         }
+        #endregion
+
+        #region GetAllRoles
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("api/User/Admin/GetAllRoles")]
+        public Dictionary<string, string> GetAllRoles()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var roles = context.Roles.ToDictionary(x => x.Id, x => x.Name);
+                return roles;
+            }
+        }
+        #endregion
+
+        #region AddRole
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/User/Admin/AddRole")]
+        public void AddRole(UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.AddToRole(pairing.UserId, pairing.RoleName);
+            }
+        }
+        #endregion
+
+        #region RemoveRole
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/User/Admin/RemoveRole")]
+        public void RemoveRole(UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.RemoveFromRole(pairing.UserId, pairing.RoleName);
+            }
+        }
+        #endregion
     }
 }
--- RMDesktopUI.Library/Api/UserEndPoint.cs
+++ RMDesktopUI.Library/Api/UserEndPoint.cs
@@ -16,6 +16,7 @@ namespace RMDesktopUI.Library.Api
             _apiHelper = apiHelper;
         }
 
+        #region GetAll
         public async Task<List<UserModel>> GetAll()
         {
             using (HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("api/User/Admin/GetAllUsers"))
@@ -31,5 +32,54 @@ namespace RMDesktopUI.Library.Api
                 }
             }
         }
+        #endregion
+
+        #region GetAllRoles
+        public async Task<Dictionary<string, string>> GetAllRoles()
+        {
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("/api/User/Admin/GetAllRoles"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<Dictionary<string, string>>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        #endregion
+
+        #region AddUserToRole
+        public async Task AddUserToRole(string userId, string roleName)
+        {
+            var data = new { userId, roleName };
+
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("/api/User/Admin/AddRole", data))
+            {
+                if (response.IsSuccessStatusCode == false)
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        #endregion
+
+        #region RemoveUserFromRole
+        public async Task RemoveUserFromRole(string userId, string roleName)
+        {
+            var data = new { userId, roleName };
+
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("/api/User/Admin/RemoveRole", data))
+            {
+                if (response.IsSuccessStatusCode == false)
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        #endregion
     }
 }
