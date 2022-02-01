815ae277a5c82af07e4d6013eba066e7b3e4f93b
Artsiom_Yurkantovich <artsiomyurkantovich@gmail.com>
2022-02-01 22:49:11 +0300

    Adde New API and Moved Over Functionlity

---
+++ RMApi/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Internal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [Authorize(Roles = "Casheir")]
+        public List<ProductModel> Get()
+        {
+
+            ProductData data = new ProductData();
+            return data.GetProducts();
+        }
+    }
+}
